4e6393567a8a0c3b45f4d64d6b25225aa1c5cd47
Petter Hohle <petter.hohle@gmail.com>
2018-07-12 15:03:37 +0200

    🐛 profile.d in separate file

---
+++ profile.d/heroku-google-cloud.sh
@@ -0,0 +1,9 @@
+# Authenticate with service account in GOOGLE_CREDENTIALS
+#!/usr/bin/env bash
+export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
+if [ -z $GOOGLE_CREDENTIALS ]; then
+    echo "GOOGLE_CREDENTIALS not set"
+else
+    echo "$GOOGLE_CREDENTIALS" | base64 -d > /app/google-credentials.json
+    gcloud auth activate-service-account --key-file /app/google-credentials.json
+fi
